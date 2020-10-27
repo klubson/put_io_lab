398740d2300267c9929de63aedd282f70f4018b1
Mateusz <mateuszk3899@wp.pl>
2020-10-27 11:27:34 +0100

    Pierwsza zmiana

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -3,7 +3,10 @@
 class Vehicle {
   int passengers; // liczba pasażerów
   int fuelcap; // pojemność zbiornika paliwa w litrach
+	//zmiana 2
   double lkm; // zużycie paliwa w litrach na 100 kilometrów
 }
 
 //kolejne zmiany
+
+//zmiana 3
